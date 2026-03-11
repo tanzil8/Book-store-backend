f7264537c08c1560fbc1786658c83c79a29a39a4
tanzil8 <khantanzil383@gmail.com>
2026-03-11 14:18:43 -0700

    backend basic setup

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+const app = express();
+const URI = process.env.MONGODB_URI
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+  
+})
+ 
+try {
+    mongoose.connect(URI)
+} catch (error) {
+    console.log(error);
+    
+}
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000')
+})
