9fa6b74e4f7c43ef5bc153e8903517e70b8c982f
Tanzil khan <khantanzil383@gmail.com>
2026-03-25 17:54:45 +0300

    index page update

--- index.js
+++ index.js
@@ -2,63 +2,48 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bookRoute from "./routers/book.router.js";
-import signupRouter from "./routers/signup.route.js"
-import loginRouter from "./routers/login.route.js"
+import signupRouter from "./routers/signup.route.js";
+import loginRouter from "./routers/login.route.js";
 
 import dns from "dns";
-import cors from "cors"
+import cors from "cors";
 
-dns.setServers(["1.1.1.1", "8.8.8.8"])
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-// const URI = process.env.MONGO_URI;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-// MongoDB connection
-// mongoose.connect(URI)
-// .then(() => {
-//   console.log("MongoDB connected");
-// })
-// .catch((error) => {
-//   console.log("MongoDB connection error:", error);
-// });
-
-
 let isConnected = false;
+
 async function connectToMongoDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI,{
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    })
+    });
     isConnected = true;
-    console.log("mongoDB connected");
-    
+    console.log("MongoDB connected");
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
   if (!isConnected) {
-    connectToMongoDB()
+    connectToMongoDB();
   }
-  next()
-})
+  next();
+});
 
 app.use("/book", bookRoute);
 app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
 
-// app.listen(3000, () => {
-//   console.log("Server is running on http://localhost:3000");
-// });
-
-module.exports = app
+export default app;
