a6282bbb18e7f68a155f96d4fa1f56ee95cbaa78
Tanzil khan <khantanzil383@gmail.com>
2026-03-25 17:48:17 +0300

    vercel deploy

--- index.js
+++ index.js
@@ -15,25 +15,50 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const URI = process.env.MONGO_URI;
+// const URI = process.env.MONGO_URI;
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
 // MongoDB connection
-mongoose.connect(URI)
-.then(() => {
-  console.log("MongoDB connected");
+// mongoose.connect(URI)
+// .then(() => {
+//   console.log("MongoDB connected");
+// })
+// .catch((error) => {
+//   console.log("MongoDB connection error:", error);
+// });
+
+
+let isConnected = false;
+async function connectToMongoDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    isConnected = true;
+    console.log("mongoDB connected");
+    
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+app.use((req, res, next)=>{
+  if (!isConnected) {
+    connectToMongoDB()
+  }
+  next()
 })
-.catch((error) => {
-  console.log("MongoDB connection error:", error);
-});
 
 app.use("/book", bookRoute);
 app.use("/signup", signupRouter);
 app.use("/login", loginRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on http://localhost:3000");
+// });
+
+module.exports = app
